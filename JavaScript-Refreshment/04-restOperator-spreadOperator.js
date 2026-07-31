a52018dd1e58835d7a209933db706f233d9394dd
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-07-31 10:19:07 +0700

    WHYY

---
+++ JavaScript-Refreshment/04-restOperator-spreadOperator.js
@@ -0,0 +1,9 @@
+function hitung(a, b, c, d, ...rest) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+    console.log(rest);
+}
+
+hitung(1, 2, 3, 4, 5, 6, 7, 8, 9,); 
