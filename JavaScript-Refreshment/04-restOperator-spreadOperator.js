0524c6e235504d3f9f083c964b486a9e5923295f
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-07-31 10:32:44 +0700

    updated script

--- JavaScript-Refreshment/04-restOperator-spreadOperator.js
+++ JavaScript-Refreshment/04-restOperator-spreadOperator.js
@@ -7,3 +7,12 @@ function hitung(a, b, c, d, ...rest) {
 }
 
 hitung(1, 2, 3, 4, 5, 6, 7, 8, 9,); 
+//* spread operator
+const ang = [1, 2, 3, 4, 5];
+const ang2 = [6, 7, 8, 9, 10];
+const obj = {...ang}
+console.log(obj);
+const arrays = [...ang, ...ang2];
+const arrays2 = [ang, ang2];
+console.log(arrays);
+console.log(arrays2);
