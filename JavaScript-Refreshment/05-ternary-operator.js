231ebe5f8ebc9f325699eb6bb8519a80ba07392b
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-07-31 10:57:05 +0700

    added

--- JavaScript-Refreshment/05-ternary-operator.js
+++ JavaScript-Refreshment/05-ternary-operator.js
@@ -1,6 +1,9 @@
-const name = "";
+const name = 0;
 if (name) {
     console.log("a")
 } else {
     console.log("b")
 }
+console.log(typeof name)
+let hasilnama = nama ? "a" : "b"
+console.log(hasilnama)
