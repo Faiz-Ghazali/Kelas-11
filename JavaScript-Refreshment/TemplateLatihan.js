a8d4bdb9851ac3fa8de2927e59a0029880067236
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-04 10:54:31 +0700

    for exam5

--- JavaScript-Refreshment/TemplateLatihan.js
+++ JavaScript-Refreshment/TemplateLatihan.js
@@ -1,5 +1,12 @@
+//* soal 1
 const umur = undefined;
 console.log(umur ?? 18);
+//* soal 2
 const stok = 5;
 let jumlahstok = stok > 0 ? "Stok Tersedia" : "Stok habis";
 console.log(jumlahstok);
+//* soal 3
+const buah1 = ["Apel", "Jeruk"];
+const buah2 = ["Mangga", "Pisang"]; 
+const semuaBuah = [...buah1, ...buah2];
+console.log(semuaBuah);
