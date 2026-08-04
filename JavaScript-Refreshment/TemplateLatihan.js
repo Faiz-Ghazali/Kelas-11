982de65fc000b80984918cc6a3949836a1f0c9a4
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-04 10:50:44 +0700

    for exam 3

--- JavaScript-Refreshment/TemplateLatihan.js
+++ JavaScript-Refreshment/TemplateLatihan.js
@@ -1,5 +1,5 @@
 const umur = undefined;
 console.log(umur ?? 18);
 const stok = 5;
-let jumlahstok = stok > 0 ? stok : "Stok habis";
+let jumlahstok = stok < 0 ? stok : "Stok habis";
 console.log(jumlahstok);
