1b3dfa0d90403ee8798f26f59634709236606c02
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-04 13:07:08 +0700

    for lesson 5

--- JavaScript-Refreshment/react-basic.js
+++ JavaScript-Refreshment/react-basic.js
@@ -1,18 +1,12 @@
-const root = document.getElementById('root');
-const added = "fulan"
-root.innerHTML = 
+
+function render(name) {
+    const root = document.getElementById('root');
+    root.innerHTML = 
 `<h1> Hello ${added} World</h1>
 <form>
 <input type="text" placeholder="Masukkan nama">
 <button type="submit">Submit</button>
 </form>
 `;
-function addedHTML() {
-    return `<h1> Hello ${added} World</h1>
-<form>
-<input type="text" placeholder="Masukkan nama">
-<button type="submit">Submit</button>
-</form>
-`;
 }
-addedHTML();
+render("fulan");
