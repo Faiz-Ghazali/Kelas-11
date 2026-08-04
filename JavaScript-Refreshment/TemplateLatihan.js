e184e7791a9a1706d8e4396eaf75839065c9c915
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-04 11:04:46 +0700

    for exam 9

--- JavaScript-Refreshment/TemplateLatihan.js
+++ JavaScript-Refreshment/TemplateLatihan.js
@@ -10,10 +10,10 @@ const buah1 = ["Apel", "Jeruk"];
 const buah2 = ["Mangga", "Pisang"]; 
 const semuaBuah = [...buah1, ...buah2];
 console.log(semuaBuah);
-function tampilkanAngka(...angka) {
-  console.log(angka);
+function tampilkanNomor(...nomor) {
+  console.log(nomor);
 }
-tampilkanAngka(1, 2, 3, 4, 5);
+tampilkanNomor(1, 2, 3, 4, 5);
 //* soal 4
 function penambahan(numberOne, numberTwo) {
     return numberOne + numberTwo;
@@ -21,3 +21,19 @@ function penambahan(numberOne, numberTwo) {
 console.log(penambahan(10, 20));
 const pertambahan = (number1, number2) => number1 + number2;
 console.log(pertambahan(5, 5));
+//* soal 5
+const angka = [10,20,30]; 
+console.log(angka[0]);
+console.log(angka[1]);
+console.log(angka[2]);
+const [angka1, angka2, angka3] = angka;
+console.log(angka1);
+const siswa = {
+    nama: "Ahmad",
+    umur: 16
+};
+console.log(siswa.nama);
+console.log(siswa.umur);
+const {nama, umur} = siswa;
+console.log(nama);
+console.log(umur);
