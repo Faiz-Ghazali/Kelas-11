abb226277d404ab9f24673673024d15a192d64a5
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-05 12:41:58 +0700

    try1

--- react.js/react2/src/App.jsx
+++ react.js/react2/src/App.jsx
@@ -1,18 +1,18 @@
-function App() {
-  
-  return (
-    <>
-      <h1>Hello React</h1>
-
-      
-    </>
-  )
-}
-export default function Halo() {
+function Halo() {
   return (
     <>
     <h1>Halo Dunia </h1>
     </>
   )
 }
+function App() {
+  
+  return (
+    <>
+      <h1>Hello React</h1>
+      <Halo> </Halo>
+    </>
+  )
+}
+
 export default App
