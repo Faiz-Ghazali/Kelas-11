f4b2b4a0153ef9f7b0bcdfae75b22be9e08d399f
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-05 12:54:11 +0700

    try3

--- react.js/react2/src/App.jsx
+++ react.js/react2/src/App.jsx
@@ -5,12 +5,31 @@ function Halo() {
     </>
   )
 }
+function Navbar() {
+  return (
+    <h1>Ini Navbar</h1>
+  )
+}
+function Hero() {
+  return (
+    <h1>Ini Hero</h1>
+  )
+}
+function Footer() {
+  return (
+    <h1>Ini Footer</h1>
+  )
+}
 function App() {
   
   return (
     <>
+      <Navbar />
       <h1>Hello React</h1>
       <Halo />
+      <Hero />
+      <Footer />
+
     </>
   )
 }
