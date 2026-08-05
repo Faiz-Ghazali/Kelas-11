95f12796c9213570bd607064ee3046fdfc779076
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-05 11:27:13 +0700

    for testing

--- react.js/react2/src/App.jsx
+++ react.js/react2/src/App.jsx
@@ -2,8 +2,10 @@ function App() {
   
   return (
     <>
-      <section>
-       
+      <section className="w-15 h-4">
+       <p className="text-red-400 text-4xl">
+        Percobaan Satu
+        </p>
       </section>
 
       
