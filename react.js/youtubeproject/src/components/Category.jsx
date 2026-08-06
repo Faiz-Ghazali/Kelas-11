b3344c2f467359f60a2bf729689321382106b7d9
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-06 11:44:31 +0700

    completed Category

--- react.js/youtubeproject/src/components/Category.jsx
+++ react.js/youtubeproject/src/components/Category.jsx
@@ -4,7 +4,7 @@ export default function Category() {
     ]
 
     return (
-        <div className="bg-neutral-800 py-3 mt-4">
+        <div className="bg-neutral-800 py-3 ">
             <div className="max-w-7xl mx-auto px-4">
                 <div className="overflow-x-auto">
                     <div className="inline-flex gap-3 items-center">
