665b57ee92c23312b684b1fae1b90de10777aca5
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-07 08:47:16 +0700

    added card

--- react.js/react2/src/App.jsx
+++ react.js/react2/src/App.jsx
@@ -23,3 +23,4 @@ function App() {
 }
 
 export default App
+//! kalau ingin custom nama bisa dengan mendefinisikan sebuah function dari import dengan perubahan function name, misal: import { default as HeroComponent } from './components/Hero.jsx'
