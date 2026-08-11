bcc68bb59768ff5b1f1b59771589076459342bc2
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-11 10:52:10 +0700

    responsive Icon

--- react.js/youtubeproject/src/components/Navbar.jsx
+++ react.js/youtubeproject/src/components/Navbar.jsx
@@ -52,7 +52,7 @@ export default function Navbar() {
                         </svg>
                     </button>
 
-                    <button className="p-2 rounded-full hover:bg-neutral-800">
+                    <button className="p-2 rounded-full hover:bg-neutral-800 sm:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                         </svg>
