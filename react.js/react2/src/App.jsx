6d6f06208eaad285c7aed3c0a204c0235455a2d4
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-12 10:25:19 +0700

    refreshed App

--- react.js/react2/src/App.jsx
+++ react.js/react2/src/App.jsx
@@ -1,25 +1,11 @@
-import Hero from './components/Hero.jsx'
-import Navbar from './components/Navbar.jsx'
-import Footer from './components/Footer.jsx'
-import Card from './components/Card.jsx'
+
 import ProfilePengguna from './components/ProfilPengguna.jsx'
-function Halo() {
-  return (
-    <>
-    <h1>Halo Dunia </h1>
-    </>
-  )
-}
+
 function App() {
   
   return (
     <>
-      <Navbar />
-      <h1>Hello React</h1>
-      <Halo />
-      <Hero />
-      <Footer />
-      <Card />
+      
       <ProfilePengguna nama="John Doe" usia="30" pekerjaan="Software Engineer" />
       <ProfilePengguna nama="Jane Smith" usia="28" pekerjaan="Designer" />
 
