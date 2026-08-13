75913385ca064fd4bcee640d1ba9ad3f3fb92779
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-13 09:57:42 +0700

    added new

--- react.js/youtubeproject/src/components/Navbar.jsx
+++ react.js/youtubeproject/src/components/Navbar.jsx
@@ -52,12 +52,6 @@ export default function Navbar() {
                         </svg>
                     </button>
 
-                    <button className="p-2 rounded-full hover:bg-neutral-800 sm:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                    </button>
-
                     <button className="p-2 rounded-full hover:bg-neutral-800">
                         <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-bell">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
