8d0982e4ca03725f049716204970688af1b8139e
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-13 10:13:59 +0700

    final

--- react.js/latihan/src/App.jsx
+++ react.js/latihan/src/App.jsx
@@ -1,9 +1,9 @@
 import ListData from './components/ListData'
 function App() {
   const santris = [
-    { id: 1, name: 'Ahmad', kelas: 12, hobi:'coding', aktif: true },
-    { id: 2, name: 'Ali', kelas: 11, hobi:'membaca', aktif: false },
-    { id: 3, name: 'Umar', kelas: 10, hobi:'Olahraga', aktif: true },
+    { id: 1, name: 'Ahmad', kelas: 'XI', hobi:'coding', aktif: true },
+    { id: 2, name: 'Ali', kelas: 'XII', hobi:'membaca', aktif: false },
+    { id: 3, name: 'Umar', kelas: 'X', hobi:'Olahraga', aktif: true },
   ]
 
   return (
