14a7e77ef99d31717960add6b37ec1d922a3137c
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-14 10:59:52 +0700

    for lesson

--- react.js/react2/src/usestate/Dasar.jsx
+++ react.js/react2/src/usestate/Dasar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 function Dasar() {
+let angka = 0;
+function tambah() {
+    angka = angka +1;
+    console.log(angka)
+}
   return (
     <div>
-      
+      <h1>Dasar useState</h1>
+      <button onClick={tambah}>Tambah</button>
     </div>
   )
 }
