2fd6146bb3348705b04fb455cd5c3522012eb480
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-18 11:02:52 +0700

    for education 2

--- react.js/react2/src/usestate/Praktik.jsx
+++ react.js/react2/src/usestate/Praktik.jsx
@@ -1,15 +1,28 @@
 import {useState} from 'react'
 
 function Praktik() {
-    const [Nomor, setNomor] = useState(1);
+    const [Nomor, setNomor] = useState(0);
     function pertambahan() {
         setNomor(Nomor + 1);
+    }
+    function kelipatanDua() {
+        setNomor(Nomor + 2);
     }
   return (
     <div>
         <h1> Nilai Awal: 1</h1>
         <button onClick={pertambahan}>
-            +
+            Tambah Satu
+        </button>
+        <br></br>
+        <button onClick={kelipatanDua}>
+            Tambah Dua
+        </button>
+        <br></br>
+        <button onClick={function KelipatanTiga() {
+            setNomor(Nomor + 3);
+        }}>
+            Tambah Tiga
         </button>
         <h1>Hasil: {Nomor}</h1>
     </div>
