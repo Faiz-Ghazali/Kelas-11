26421328a5269c03cd343db7941fc06bb9969117
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-18 10:28:44 +0700

    for education

--- react.js/react2/src/AppUseState.jsx
+++ react.js/react2/src/AppUseState.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+// import React from 'react'
 import Dasar from './usestate/Dasar.jsx'
 import MenyimpanData from './usestate/MenyimpanData.jsx'
 function AppUseState() {
   return (
     <div>
       <Dasar />
+      <MenyimpanData />
     </div>
   )
 }
--- react.js/react2/src/usestate/MenyimpanData.jsx
+++ react.js/react2/src/usestate/MenyimpanData.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-
+import  { useState } from 'react'
 function MenyimpanData() {
 
     const [Angka, setAngka] = useState(0);
