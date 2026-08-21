a918d2c78897c776a959ba7e2a9f472b67851050
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-21 11:13:55 +0700

    for added

--- react.js/react2/src/StyleComponents/RadixPrimitives.jsx
+++ react.js/react2/src/StyleComponents/RadixPrimitives.jsx
@@ -1,42 +1,54 @@
-import { DropdownMenu, Button, Theme } from "@radix-ui/themes";
+import { DropdownMenu, Button, Theme, Text, HoverCard, Flex, Link, Box, Avatar, Heading} from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 function RadixPrimitives() {
-  return (
+return (
     <div>
-      <DropdownMenu.Root>
-	<DropdownMenu.Trigger>
-		<Button variant="soft">
-			Options
-			<DropdownMenu.TriggerIcon />
-		</Button>
-	</DropdownMenu.Trigger>
-	<DropdownMenu.Content>
-		<DropdownMenu.Item shortcut="⌘ E">Edit</DropdownMenu.Item>
-		<DropdownMenu.Item shortcut="⌘ D">Duplicate</DropdownMenu.Item>
-		<DropdownMenu.Separator />
-		<DropdownMenu.Item shortcut="⌘ N">Archive</DropdownMenu.Item>
+    <Theme>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger asChild>
+            <Button variant="soft">Options</Button>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Item>Edit</DropdownMenu.Item>
+            <DropdownMenu.Item>Duplicate</DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      </Theme>
+      <Text>
+	Follow{" "}
+	<HoverCard.Root>
+		<HoverCard.Trigger>
+			<Link href="https://twitter.com/radix_ui" target="_blank">
+				@radix_ui
+			</Link>
+		</HoverCard.Trigger>
+		<HoverCard.Content maxWidth="300px">
+			<Flex gap="4">
+				<Avatar
+					size="3"
+					fallback="R"
+					radius="full"
+					src="https://pbs.twimg.com/profile_images/1337055608613253126/r_eiMp2H_400x400.png"
+				/>
+				<Box>
+					<Heading size="3" as="h3">
+						Radix
+					</Heading>
+					<Text as="div" size="2" color="gray" mb="2">
+						@radix_ui
+					</Text>
+					<Text as="div" size="2">
+						React components, icons, and colors for building high-quality,
+						accessible UI.
+					</Text>
+				</Box>
+			</Flex>
+		</HoverCard.Content>
+	</HoverCard.Root>{" "}
+	for updates.
+</Text>
 
-		<DropdownMenu.Sub>
-			<DropdownMenu.SubTrigger>More</DropdownMenu.SubTrigger>
-			<DropdownMenu.SubContent>
-				<DropdownMenu.Item>Move to project…</DropdownMenu.Item>
-				<DropdownMenu.Item>Move to folder…</DropdownMenu.Item>
-
-				<DropdownMenu.Separator />
-				<DropdownMenu.Item>Advanced options…</DropdownMenu.Item>
-			</DropdownMenu.SubContent>
-		</DropdownMenu.Sub>
-
-		<DropdownMenu.Separator />
-		<DropdownMenu.Item>Share</DropdownMenu.Item>
-		<DropdownMenu.Item>Add to favorites</DropdownMenu.Item>
-		<DropdownMenu.Separator />
-		<DropdownMenu.Item shortcut="⌘ ⌫" color="red">
-			Delete
-		</DropdownMenu.Item>
-	</DropdownMenu.Content>
-</DropdownMenu.Root>
-
+       
     </div>
   )
 }
