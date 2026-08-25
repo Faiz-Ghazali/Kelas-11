1fa4783806c8087e080f005adba52179706f979c
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-25 10:33:53 +0700

    for update

--- react.js/react2/src/StyleComponents/RadixPrimitives.jsx
+++ react.js/react2/src/StyleComponents/RadixPrimitives.jsx
@@ -1,4 +1,4 @@
-import { DropdownMenu, Button, Theme, Text, HoverCard, Flex, Link, Box, Avatar, Heading} from "@radix-ui/themes";
+import { DropdownMenu, Button, Theme, Text, Quote} from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 function RadixPrimitives() {
 return (
@@ -15,37 +15,9 @@ return (
         </DropdownMenu.Root>
       </Theme>
       <Text>
-            Follow{" "}
-            <HoverCard.Root>
-                <HoverCard.Trigger>
-                    <Link href="https://twitter.com/radix_ui" target="_blank">
-                        @radix_ui
-                    </Link>
-                </HoverCard.Trigger>
-                <HoverCard.Content maxWidth="300px">
-                    <Flex gap="4">
-                        <Avatar
-                            size="3"
-                            fallback="R"
-                            radius="full"
-                            src="https://pbs.twimg.com/profile_images/1337055608613253126/r_eiMp2H_400x400.png"
-                        />
-                        <Box>
-                            <Heading size="3" as="h3">
-                                Radix
-                            </Heading>
-                            <Text as="div" size="2" color="gray" mb="2">
-                                @radix_ui
-                            </Text>
-                            <Text as="div" size="2">
-                                React components, icons, and colors for building high-quality,
-                                accessible UI.
-                            </Text>
-                        </Box>
-                    </Flex>
-                </HoverCard.Content>
-            </HoverCard.Root>{" "}
-            for updates.
+            His famous quote,{" "}
+            <Quote>Styles come and go. Good design is a language, not a style</Quote>,
+            elegantly summs up Massimos philosophy of design.
         </Text>
 
        
